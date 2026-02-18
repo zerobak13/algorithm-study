56fc7ad36380c34fa9d7ef1b26697d619b22cd69
박제영 <143867779+zerobak13@users.noreply.github.com>
2026-02-18 12:55:58 +0900

    [level 2] Title: 피로도, Time: 3.99 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,30 @@
+class Solution {
+    static boolean []visited;
+    static int result=Integer.MIN_VALUE;
+    static int n;
+    public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        int count=0;
+        n=dungeons.length;
+        visited=new boolean[n];
+        dfs(k,dungeons,0,0);
+        answer=result;
+        return answer;
+    }
+    static void dfs(int k,int[][]dungeons, int count, int depth){
+        
+        result=Math.max(result,count);      
+        
+        for(int i=0;i<n;i++){
+             if(!visited[i]){
+                 if(dungeons[i][0]<=k){
+                     visited[i]=true;
+                     dfs(k-dungeons[i][1], dungeons,count+1,depth+1);
+                     visited[i]=false;
+                 }
+             }          
+        }
+       
+        
+    }
+}
